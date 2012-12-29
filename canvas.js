e8f7489a170cb3f192a5461d24d8a1873000add6
Chris <christine.ann.odell@gmail.com>
2012-12-29 00:03:44 +0000

    Moved the plane around a bit for the fractal and it finally looks like a Mandelbrot

--- canvas.js
+++ canvas.js
@@ -9,34 +9,31 @@ function Fractal() {
 
     this.render = function() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+        var shiftX = 100+(canvas.width/2);
+        var shiftY = canvas.height/2;
 
-        for (var i = 1; i <= 300; i++){
-            for (var j = 1; j <= 150; j++){
-                var c1 = (-2+4)*i/300;
-                var c2 = (2-4)*j/300;
+        for (var i = -shiftX; i <= canvas.width-shiftX; i++){
+            for (var j = -shiftY; j <= (canvas.height-shiftY); j++){
+                var c1 = (-2+4)*i/canvas.width;
+                var c2 = (2-4)*j/canvas.height;
                 var x = c1;
                 var y = c2;
 
-//                console.log("c1 " + c1);
-
-                for (var n = 1; n <= 10; n++){
+                for (var n = 1; n <= 20; n++){
                     var x1=(x*x)-(y*y)+c1;
                     var y1=2*x*y+c2;
                     var r=x1*x1+y1*y1;
                     if(r > 4){
-                        console.log("break! " + r + " i: " + i + " j: " + j);
+//                        console.log("break! " + r + " i: " + i + " j: " + j);
                         break;
                     }
                     x = x1;
                     y = y1;
-                    var fillStyle = 'rgb(' + (n*10) + ',0,0)';
+                    var fillStyle = 'rgb(' + (n*20) + ',0,0)';
                     ctx.fillStyle = fillStyle;
                 }
 
-//                console.log("Draw!");
-//                console.log(fillStyle);
-                ctx.fillRect(i, j, 1, 1);
-                ctx.fillRect(i, 300-j, 1, 1);
+                ctx.fillRect(i+shiftX, j+shiftY, 1, 1);
             }
         }
     };
